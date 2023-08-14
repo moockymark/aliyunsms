8acc627785695181f540581fd5801fbdd1582671
moocky <moocky@moocky.net>
2023-08-14 10:47:18 +0800

    修改路径

--- src/Aliyunsms.php
+++ src/Aliyunsms.php
@@ -11,59 +11,52 @@ use Illuminate\Config\Repository;
 
 class Aliyunsms
 {
-    protected $config;
+	protected $config;
 
-    public function __construct(Repository $config)
-    {
-//        $this->config = $config;
-        $this->config = config('aliyunsms.options');
-    }
+	public function __construct(Repository $config)
+	{
+		$this->config = $config;
+		//$this->config = config('aliyunsms');
+	}
 
-    public function test_rtn($msg = ''){
-        return $msg.' <strong>from your custom develop package!</strong>>';
-    }
-
-
-
-
-
-
-
-    public function sendSms($phone, $signName, $templateCode, $templateParam)
-    {
-        $params = array();
-        // fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
-        // fixme 必填: 短信接收号码
-        $params["PhoneNumbers"] = strval($phone);
-        // fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
-        $params["SignName"] = $signName;
-        // fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
-        $params["TemplateCode"] = $templateCode;
-        // fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
-        $params['TemplateParam'] = $templateParam;
-        // fixme 可选: 设置发送短信流水号
-        $params['OutId'] = "12345";
-        // fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
-        $params['SmsUpExtendCode'] = "1234567";
-        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
-        if (!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
-            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
-        }
-        // 此处可能会抛出异常，注意catch
-        $content = $this->request(
-            $this->config['access_key_id'],
-            $this->config['access_key_secret'],
-            "dysmsapi.aliyuncs.com",
-            array_merge($params, array(
-                "RegionId" => "cn-hangzhou",
-                "Action" => "SendSms",
-                "Version" => "2017-05-25",
-            ))
-        // fixme 选填: 启用https
-        // ,true
-        );
-        return $content;
-    }
+	public function test_rtn($msg = ''){
+		return $msg.' <strong>from your custom develop package!</strong>>';
+	}
+	public function sendSms($phone, $signName, $templateCode, $templateParam)
+	{
+		$params = array();
+		// fixme 必填: 请参阅 https://ak-console.aliyun.com/ 取得您的AK信息
+		// fixme 必填: 短信接收号码
+		$params["PhoneNumbers"] = strval($phone);
+		// fixme 必填: 短信签名，应严格按"签名名称"填写，请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/sign
+		$params["SignName"] = $signName;
+		// fixme 必填: 短信模板Code，应严格按"模板CODE"填写, 请参考: https://dysms.console.aliyun.com/dysms.htm#/develop/template
+		$params["TemplateCode"] = $templateCode;
+		// fixme 可选: 设置模板参数, 假如模板中存在变量需要替换则为必填项
+		$params['TemplateParam'] = $templateParam;
+		// fixme 可选: 设置发送短信流水号
+		$params['OutId'] = "12345";
+		// fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+		$params['SmsUpExtendCode'] = "1234567";
+		// *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+		if (!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+				$params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+		}
+		// 此处可能会抛出异常，注意catch
+		$content = $this->request(
+				$this->config['access_key_id'],
+				$this->config['access_key_secret'],
+				"dysmsapi.aliyuncs.com",
+				array_merge($params, array(
+						"RegionId" => "cn-hangzhou",
+						"Action" => "SendSms",
+						"Version" => "2017-05-25",
+				))
+		// fixme 选填: 启用https
+		// ,true
+		);
+		return $content;
+	}
 
     /**
      * 生成签名并发起请求
